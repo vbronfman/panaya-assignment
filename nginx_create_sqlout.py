6a57259d01197adae0b483e78d2b20acbaa4b597
vlad bronfman <root_admin@PCBANG>
2023-11-04 15:30:47 +0200

    python input args added

--- nginx_create_sqlout.py
+++ nginx_create_sqlout.py
@@ -2,15 +2,36 @@ from mysql import connector
 import pandas as pd
 import argparse
 
+#Process input if any
 
-htmlfile = "nginx_static.html"
+# Construct the argument parser
+ap = argparse.ArgumentParser()
+
+# Add the arguments to the parser
+ap.add_argument("-u", "--mysqluser", default="report",
+   help="MySQL read user")
+ap.add_argument("-p", "--mysqluserpass", default="report",
+   help="MySQL read user password")
+ap.add_argument("-b", "--database", default="DB",
+   help="MySQL databast name")
+ap.add_argument("-d", "--dbhost", default="localhost",
+   help="DB host. Try either localhost or host.docker.internal")
+ap.add_argument("-o", "--out", default="nginx_static.html",
+   help="Static output from database")
+
+args = vars(ap.parse_args())
+
+htmlfile = args.get("out","nginx_static.html")
 
 #https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
 mydb =  connector.connect(
-  host="localhost",
-  user="report",
+  #host="localhost",
+  host = args.get("dbhost"), #"172.19.244.87",
+  user=args.get("mysqluser"),
+  #user="report",
   password="report",
-  database="DB")
+  database=args.get("database","DB")
+)
 
 mycursor = mydb.cursor()
 
@@ -42,9 +63,6 @@ join  As_project        as p
 )
 
 myresult = mycursor.fetchall()
-
-for x in myresult:
-  print (f"DEBUG {x}")
 
 # https://stackoverflow.com/questions/63487038/formatting-html-pandas-tables-in-python
 df = pd.DataFrame(myresult,columns='Company_ID Company_Name Account_ID Account_Name Project_ID Project_Name Project_Status'.split())
